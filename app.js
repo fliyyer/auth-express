5eb5330be92bb2a5f6ae2314825201bb98689428
rahmatraywhite <rahmat.hidayat@raywhite.co.id>
2025-03-05 10:48:35 +0700

    connected to mongo

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 import express from "express"
+import mongoose from "mongoose"
+import 'dotenv/config'
 const app = express()
-const port = 3000
+const port = process.env.PORT
 import AuthRoutes from './routes/authRoutes.js'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.use('/api/v1/auth', AuthRoutes);
+
+try {
+  await mongoose.connect(process.env.DATABASE);
+  console.log('Database Connected')
+} catch (error) {
+  handleError(error);
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
